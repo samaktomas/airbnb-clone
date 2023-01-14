c2c578f7b010dd8e9ab9a99bae775773ae5b1f15
Tomas Samak <samaktomas@gmail.com>
2023-01-14 11:24:25 +0100

    update

--- next.config.js
+++ next.config.js
@@ -3,8 +3,4 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "images.contentstack.io"],
   },
-  env: {
-    mapbox_key:
-      "pk.eyJ1Ijoic2FtYWt0b21hcyIsImEiOiJjbGN1cjV1eTcxMjZmM3NwMXMzaGR0aDR5In0.eztdA1fEc3zK86MoNOdEkw",
-  },
 };
